e3a89fea8a1f5184e070aa4df429d31bd293fd36
Boru Isako <isco.jaldy@gmail.com>
2025-05-19 17:24:56 +0300

    Completed labs

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'Male', 'White', 'aggressive', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'Male', 'Brown', 'aggressive', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'Female', 'Black', 'playful', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'Male', 'Dark Brown', 'calm', FALSE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'Female', 'Cinnamon', 'shy', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'Male', 'Grey', 'grumpy', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'Female', 'White', 'curious', TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'Male', 'Black', 'mysterious', FALSE);
